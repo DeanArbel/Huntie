1cd6c0859a7730490f2ff2e5cb0dc68741a17f8b
DeanArbel <deanarbel@gmail.com>
2017-02-17 18:37:57 +0200

    Finished Game Type window

--- web/js/GameCreator/GameType.js
+++ web/js/GameCreator/GameType.js
@@ -1,6 +1,70 @@
 /**
  * Created by Dean on 17/2/2017.
  */
+var TEAM_GAME = "Team Game";
+var INDIVIDUAL_GAME = "Individual Game";
+var MAX_GROUPS = 10;
+var mTeams = {};
+var sCurGameType;
+var groupNameInput;
+
+$(function() {
+    dropdownChange($('.dropdown-btn')[0].innerText);
+    document.getElementById('groupName').oninput = onTeamNameInput;
+    groupNameInput = $('#groupName')[0];
+});
+
+function onTeamNameInput() {
+    var inputValue = groupNameInput.value;
+    $('#addTeam-btn')[0].disabled = mTeams[inputValue];
+}
+
 function dropdownChange(innerText) {
     console.log("dropdown-btn value is now " + innerText);
+    sCurGameType = innerText;
+    if (innerText === TEAM_GAME) {
+        $('.show-team').show();
+        $('.show-individual').hide();
+    }
+    else {
+        $('.show-team').hide();
+        $('.show-individual').show();
+    }
+}
+
+$(document).on('click','#openModal-btn', function() {
+    $('#addTeam-btn')[0].disabled = true;
+});
+
+$(document).on('shown.bs.modal', '#myModal', function () {
+    $('#addTeam-btn')[0].disabled = true;
+    groupNameInput.focus();
+});
+
+$(document).on('click', '#addTeam-btn', function() {
+    var inputValue = groupNameInput.value;
+    mTeams[inputValue] = {};
+    groupNameInput.value = "";
+    addRow(inputValue);
+});
+
+function addRow(sRowName) {
+    var eTbody = $('table > tbody');
+    var $eRow = $('<tr>');
+    $eRow.append('<td>' + sRowName + '</td>');
+    $eRow.append(REMOVE_BUTTON_SVG);
+    eTbody.append($eRow);
+    if (_isTableFull()) {
+        $('#openModal-btn')[0].disabled = true;
+    }
+}
+
+function rowWasRemoved(that) {
+    var removedValue = $(that).parents('td').siblings()[0].innerText;
+    delete mTeams[removedValue];
+    $('#openModal-btn').disabled = false;
+}
+
+function _isTableFull() {
+    return $('table > tbody').children().length >= MAX_GROUPS;
 }
--- web/js/Util/UtilityFuncs.js
+++ web/js/Util/UtilityFuncs.js
@@ -1,15 +1,21 @@
 /**
  * Created by Dean on 17/2/2017.
  */
-$(document).on('click', 'svg', function() {
+var CARET_PROPERTY = ' <span class="caret"></span>';
+var REMOVE_BUTTON_SVG = '<td><svg> <circle cx="12" cy="12" r="11" stroke="black" stroke-width="2" fill="white" /> <path stroke="black" stroke-width="4" fill="none" d="M6.25,6.25,17.75,17.75" /> <path stroke="black" stroke-width="4" fill="none" d="M6.25,17.75,17.75,6.25" /> </svg></td>';
+
+$(document).on('click', 'td > svg', function() {
     $(this).parents('tr').remove();
+    rowWasRemoved(this);
 });
 
 $(document).on('click', '.dropdown-selection', function() {
     var value = $(this)[0].innerText,
-        dropdownBtn = $('.dropdown-btn')[0];
-    if (value !== dropdownBtn.innerText) {
-        dropdownBtn.innerText = $(this)[0].innerText;
-        dropdownChange(dropdownBtn.innerText);
+        dropdownBtn = $('.dropdown-btn');
+    if (value !== dropdownBtn[0].innerText) {
+        dropdownBtn[0].innerText = $(this)[0].innerText;
+        dropdownBtn.append(CARET_PROPERTY);
+        dropdownChange(value);
     }
 });
+
