c9f358c97ef964ad45df7b33c2ef8a22363c9386
Dean Arbel <d.arbel@sap.com>
2017-09-05 19:39:53 +0300

    strange commit fix

--- web/js/User/home.js
+++ web/js/User/home.js
@@ -7,13 +7,11 @@ $(document).on('click', "#createGame-btn", function() {
        type: 'POST',
        data: {token: sessionStorage.getItem("access token")},
        success: function(response) {
-           window.location.href =   "/Manager/game-type.html";
+           window.location.href =   "/Manager/GameType.html";
        },
        error: function(xhr, status, error) {
            if (xhr.status === 499) {
                $("#myModal").modal('show');
-           } else if (xhr.status === 480) {
-               window.location.href = "/index.jsp";
            }
            else if (xhr.status === 400) {
                console.log(xhr.getResponseHeader(error));
@@ -23,21 +21,21 @@ $(document).on('click', "#createGame-btn", function() {
 });
 
 $(document).on('click', "#findGame-btn", function() {
-    window.location.href =   "/Player/find-game.html";
+    window.location.href =   "/Player/FindGame.html";
 });
 
 $(document).on('click', "#profile-btn", function() {
-    window.location.href =   "/my-profile.html";
+    window.location.href =   "/MyProfile.html";
 });
 
-$(document).on('click', '#modal-yes-btn', function() { window.location.href =   "/Manager/game-type.html"; });
+$(document).on('click', '#modal-yes-btn', function() { window.location.href =   "/Manager/GameType.html"; });
 $(document).on('click', '#modal-no-btn', function() {
     $.ajax({
         url: "newGame",
         type: 'POST',
         data: { createNewGame: true, token: sessionStorage.getItem("access token") },
         success: function(response) {
-            window.location.href =   "/Manager/game-type.html";
+            window.location.href =   "/Manager/GameType.html";
         },
         error: function(xhr, status, error) {
             if (xhr.status === 400) {
